bd88a79244e88bb495fa4a2f293e759beaf965ae
prossellob <prosselloboix@gmail.com>
2020-04-04 16:12:41 +0200

    Falta unir la parte de Reddit

--- bot.py
+++ bot.py
@@ -1,10 +1,28 @@
-import tweepy
+import tweepy,time
 
 auth = tweepy.OAuthHandler("", "")
 auth.set_access_token("", "")
 
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True,
+    wait_on_rate_limit_notify=True)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+ # api.update_status("This is just a test.") How to publish a tweet.
+
+# MAIN
+while True:
+    mentions = api.mentions_timeline();
+    txt = open("test.txt","r+")
+    last_id = int(txt.readline())
+    for m in mentions:
+        if m.id > last_id:
+            txt = open("test.txt","w")
+            txt.write(str(m.id))
+            txt.close()
+            user = m.user.id
+            x = api.get_user(user)
+            print("ID tweet: " + str(m.id))
+            print("ID usuario: " +str(user))
+            print("Nombre usuario: " + x.screen_name)
+            api.update_status("Hello " + '@' + str(x.screen_name),in_reply_to_status_id = m.id)
+    print("Sleeping...")
+    time.sleep(60)
