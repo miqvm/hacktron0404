4dd841d59b440e9287c7c459b8ec01a59917e006
prossellob <prosselloboix@gmail.com>
2020-04-04 12:58:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import tweepy
 
-auth = tweepy.OAuthHandler("rpHOqNQI8PIDj8AaSMKlB417K", "cgQaYKW6tcYn8Dn245FWayjrDAEoi32OhhVOHFayylbaQsKEaj")
-auth.set_access_token("1246387201610518528-8yvIpEjWKScPpjod9sCqOig8y7PHV2", "X6mdi2HLdvKM3NjYNeaCBJTeiakatajPQaTlPtk0KfMHk")
+auth = tweepy.OAuthHandler("", "")
+auth.set_access_token("", "")
 
 api = tweepy.API(auth)
 
