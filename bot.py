f5ee1b5d069a9b11aed9ace47a68e43e2b56fd59
miqvm <61425828+miqvm@users.noreply.github.com>
2020-04-04 19:29:10 +0200

    cosetes

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import tweepy,time,os
 import getImgReddit as reddit
 
-auth = tweepy.OAuthHandler("", "")
-auth.set_access_token("", "")
+auth = tweepy.OAuthHandler("rpHOqNQI8PIDj8AaSMKlB417K", "cgQaYKW6tcYn8Dn245FWayjrDAEoi32OhhVOHFayylbaQsKEaj")
+auth.set_access_token("1246387201610518528-8yvIpEjWKScPpjod9sCqOig8y7PHV2", "X6mdi2HLdvKM3NjYNeaCBJTeiakatajPQaTlPtk0KfMHk")
 
 api = tweepy.API(auth, wait_on_rate_limit=True,
     wait_on_rate_limit_notify=True)
@@ -44,11 +44,12 @@ while True:
             while(correct == 0):
                             
                 image = reddit.getImage()
-                if(reddit.acceptType(image) == 1):
+                ext = image[-3] + image[-2] + image[-1]
+                if(ext == "png" or ext == "jpg"):
                     print(image)
                     api.update_with_media("img/" + str(image),status = "@" + str(x.screen_name),in_reply_to_status_id = m.id)
                     os.system("rm img/" +str(image))
                     correct = 1
     print("Sleeping...")
-    ++i
+    i += 1
     time.sleep(60) #Pause to avoid rate limits.
