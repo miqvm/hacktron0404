493ff74308248af226b2530871729a6cccea8525
mvives <miquelvm2000@gmail.com>
2020-04-05 20:10:26 +0200

    excepcions

--- bot.py
+++ bot.py
@@ -14,45 +14,50 @@ api = tweepy.API(auth, wait_on_rate_limit=True,
 now = datetime.datetime.now()
 mnt = int(now.minute)
 while True:
-    print(mnt);
-    if mnt == 0:
-        correct = 0
-        while(correct == 0):
-            
-            image = reddit.getImage()
-            ext = image[-3] + image[-2] + image[-1]
-            if(ext == "png" or ext == "jpg"):
-                print("Normal upload: " + image)
-                api.update_with_media("img/" + str(image))
-                os.system("rm img/" +str(image))
-                correct = 1
-                
-    mentions = api.mentions_timeline();
-    txt = open("test.txt","r+")
-    last_id = int(txt.readline())
-    for m in mentions:
-        if m.id > last_id:
-            txt = open("test.txt","w")
-            txt.write(str(m.id))
-            txt.close()
-            user = m.user.id
-            x = api.get_user(user)
-            print("tweet ID: " + str(m.id))
-            print("user ID: " +str(user))
-            print("User name: " + x.screen_name)
-            
+    try:
+        print(mnt);
+        if mnt == 0:
             correct = 0
             while(correct == 0):
-                            
+                
                 image = reddit.getImage()
                 ext = image[-3] + image[-2] + image[-1]
                 if(ext == "png" or ext == "jpg"):
-                    print(image)
-                    api.update_with_media("img/" + str(image),status = "@" + str(x.screen_name),in_reply_to_status_id = m.id)
+                    print("Normal upload: " + image)
+                    api.update_with_media("img/" + str(image))
                     os.system("rm img/" +str(image))
                     correct = 1
-    print("Sleeping...")
-    mnt = mnt + 1
-    if mnt >= 120:
-        mnt = 0
-    time.sleep(60) #Pause to avoid rate limits.
+                    
+        mentions = api.mentions_timeline();
+        txt = open("test.txt","r+")
+        last_id = int(txt.readline())
+        for m in mentions:
+            if m.id > last_id:
+                txt = open("test.txt","w")
+                txt.write(str(m.id))
+                txt.close()
+                user = m.user.id
+                x = api.get_user(user)
+                print("tweet ID: " + str(m.id))
+                print("user ID: " +str(user))
+                print("User name: " + x.screen_name)
+                
+                correct = 0
+                while(correct == 0):
+                                
+                    image = reddit.getImage()
+                    ext = image[-3] + image[-2] + image[-1]
+                    if(ext == "png" or ext == "jpg"):
+                        print(image)
+                        api.update_with_media("img/" + str(image),status = "@" + str(x.screen_name),in_reply_to_status_id = m.id)
+                        os.system("rm img/" +str(image))
+                        correct = 1
+        print("Sleeping...")
+        mnt = mnt + 1
+        if mnt >= 120:
+            mnt = 0
+        time.sleep(60) #Pause to avoid rate limits.
+        
+    except Exception as e:
+        print(e)
+
